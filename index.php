ecdf635175ca1711523529e0d1e04cddf4d178d5
Rahmad Wijaya <akujaya14@gmail.com>
2018-04-02 12:47:19 +0700

    fix

--- index.php
+++ index.php
@@ -24,9 +24,13 @@
 			<div class="content-body">
 				<div class="wrapper">
 					<h1>Home</h1>
-					<a href="index.php" style="text-decoration: none; color: #aaa;">Home </a>
-					<!-- <h2>BUAT JUTAAN ORANG DI DUNIA MENEMUKAN BISNIS ANDA DI INTERNET</h2> -->
-					<!-- <p>Sebagai jasa pembuatan website profesional, Solusi Digital adalah partner terbaik untuk bisnis Anda dalam menghadapi Era Digital yang serba teknologi. Perluas jangkauan bisnis Anda, jadikan bisnis Anda sebagai bisnis modern yang mudah dijangkau semua orang di Indonesia.</p> -->
+					<a href="index.php" style="text-decoration: none; color: #aaa;">Home</a>
+					<h3>Destinasi Wisata Pantai Di Gunung Kidul</h3>
+					<p>Maju Mundur Tour Travel menyediakan paket wisata Salah satunya adalah:</p>
+
+					<p>Pantai Indrayanti Gunung Kidul Berbatasan langsung dengan Samudera Pasifik membuat Gunung Kidul ini kaya akan pantai yang Indah dan berpasir putih. Bila kita telusuri sepanjang garis pantai ini, maka kita kan menjumpai banyak sekali pantai yang ada di wilayah ini.</p> <img src="aaa.jpg" width="100%">
+
+					<p>Seperti pantai pada umumnya di GunungKidul, pantai Indyaranti ini memiliki hamparan pasir putih dengan laut yang berwarna biru yang jernih. Pantai Indrayanti ini merupakan salah satu tujuan wisata atau objek wisata yangtelah mengalami sentuhan modern oleh tangan pemerintah berupa infrastuktur dan juga fasilitas-fasilitas untuk para wisatawan.Di sepanjang bibir pantai Indrayanti ini kita dapat menemukan deretan restoran yangmenyajikan menu khas laut dan juga kelapa muda.Selain itu di pantai Indrayanti ini juga disediakan beberapa cottage yang dapat Anda gunakan jika ingin menghabiskan waktu malam Anda di pantai ini dan menikmati keindahan malam di pantai ini. Pantai Indrayanti ini juga terkenal sebagai pantai yang roamantis dan merupakan tempat wisata yang cocok untuk anak muda. Pantai Indrayanti ini beralamat di Desa Tepus, Kecamatan Tepus, Kabupaten Gunung kidul.</p>
 				</div>
 			</div>
 			<div class="sidebar">
@@ -44,7 +48,10 @@
 				<br>
 				<br>
 				<label><b>Info</b></label><br>
-				<span>Jadwal Workshop: 29 Febuari 2019</span>
+				<?php
+				$info = "Jadwal Workshop: 29 Febuari 2019";
+				echo '<label><b>'.$info.'</b></label><br>'
+				?>
 				</div>
 			</div>
 		</div>
